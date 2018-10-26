57e9e27f5d48076d1cd7bb129eccc7f6cdcc444c
Daniil <d.doronkin@itransition.com>
2018-10-26 11:28:44 +0300

    Initialize

---
+++ TimeZoneConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeZoneConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
