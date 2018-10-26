bf0b693f07afe55631a4dc4f50f7e36eafbeccc3
Daniil <d.doronkin@itransition.com>
2018-10-26 14:18:56 +0300

    removed tips

--- TimeZoneConvert/Converter.cs
+++ TimeZoneConvert/Converter.cs
@@ -11,23 +11,7 @@ namespace TimeZoneConvert
 
             try
             {
-                /*
-                    .Net Core using system time zones, but, unfortunately, Windows and Linux have different ones.
-                    To avoid potentially problems with a Linux host we try to check Windows TimeZoneID, and if we can't find suitable one - use Linux TimeZoneId
-                    https://stackoverflow.com/questions/41566395/timezoneinfo-in-net-core-when-hosting-on-unix-nginx?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-                */
-
                 TimeZoneInfo zone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
-                //try
-                //{
-                //    zone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
-                //}
-                //catch (TimeZoneNotFoundException)
-                //{
-                //    var ianaTimeZoneId = TZConvert.WindowsToIana(timeZoneId);
-                //    zone = TimeZoneInfo.FindSystemTimeZoneById(ianaTimeZoneId);
-                //}
-
                 toValue = TimeZoneInfo.ConvertTime(fromValue, TimeZoneInfo.Local, zone);
 
                 return true;
